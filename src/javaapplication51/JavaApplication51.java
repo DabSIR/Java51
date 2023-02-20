f2195219c71e2429f9e2b45f64afd5912c6bac99
Dylan Dobbs <dobbsd@sheridancollege.ca>
2023-02-20 16:18:18 -0500

    created new code

---
+++ src/javaapplication51/JavaApplication51.java
@@ -0,0 +1,16 @@
+package javaapplication51;
+
+/**
+ *
+ * @author Dylan
+ */
+public class JavaApplication51 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
