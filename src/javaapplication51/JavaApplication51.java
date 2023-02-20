c2162c702d0914266386aeb74aa1b97f0cc8019f
Dylan Dobbs <dobbsd@sheridancollege.ca>
2023-02-20 16:20:07 -0500

    added a print call

--- src/javaapplication51/JavaApplication51.java
+++ src/javaapplication51/JavaApplication51.java
@@ -10,7 +10,7 @@ public class JavaApplication51 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        System.out.println("Hello");
     }
     
 }
