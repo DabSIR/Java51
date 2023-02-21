d7bfa53e99d79c41123bf7f59b7768a0c664ad75
Dylan Dobbs <dobbsd@sheridancollege.ca>
2023-02-21 08:19:04 -0500

    added a appliance class/file

---
+++ src/javaapplication51/Appliance.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication51;
+
+/**
+ *
+ * @author Dylan
+ */
+class Appliance {
+    private String name;
+    private double powerUsage;
+    private boolean power;
+    
+    public Appliance(String name, double powerUsage) {
+        this.name = name;
+        this.powerUsage = 100;
+    }
+}
